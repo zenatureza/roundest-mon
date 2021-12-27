36daf92c855c15564e2f5c8730c0439544e17f9e
Árthur Tolfo Pinheiro <arthur.pinheiro@elsys.com.br>
2021-12-27 19:38:25 -0300

    Send back way less data

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -1,17 +1,16 @@
 import * as trpc from '@trpc/server';
 import { z } from 'zod';
+import { PokemonClient } from 'pokenode-ts';
 
-export const appRouter = trpc.router().query('hello', {
-  input: z
-    .object({
-      text: z.string().nullish(),
-    })
-    .nullish(),
-  resolve({ input }) {
-    return {
-      greeting: `hello ${input?.text ?? 'world'}`,
-    };
-  },
+export const appRouter = trpc.router().query('get-pokemon-by-id', {
+  input: z.object({id: z.number()}),
+  async resolve({ input }) {
+    const api = new PokemonClient();
+
+    const pokemon = await api.getPokemonById(input.id);
+
+    return {name: pokemon.name, sprites: pokemon.sprites};
+  } 
 });
 
 // export type definition of API
